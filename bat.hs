d3ceb3a1a25b1d8b3808020e83b34df35fda4f0e
foobardam <foobardam@gmail.com>
2012-07-10 06:11:00 +0900

    keep

--- bat.hs
+++ bat.hs
@@ -1,11 +1,28 @@
 import System.IO
+import Text.Regex.Posix
+
+getNumber :: String -> Double
+getNumber str = read (str =~ "[0-9]+" :: String) :: Double
+
+getCapacitance1 :: String -> Double
+getCapacitance1 = getNumber . head . filter (=~ "remaining capacity:") . lines 
+
+getCapacitance2 :: String -> Double
+getCapacitance2 = getNumber . head . filter (=~ "design capacity:") . lines
+
+
+
 
 main = do
-     state <- readFile "/proc/acpi/battery/BAT0/state"
-     print . lines $ state
-     info <- readFile "/proc/acpi/battery/BAT0/info"
-     print . lines $ info
-    
+  state <- readFile "/proc/acpi/battery/BAT0/state"
+  print $ getCapacitance1 state 
+  info <- readFile "/proc/acpi/battery/BAT0/info"
+  print $ getCapacitance2 info 
+
+
+
+
+  
      
 
 
