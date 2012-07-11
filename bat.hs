89e3156ddbb8b3866414afa15412a8d541da294d
foobardam <foobardam@gmail.com>
2012-07-12 00:02:29 +0900

    modify

--- bat.hs
+++ bat.hs
@@ -9,8 +9,8 @@ getCapacitance ptn lns = getNumber . head . filter (=~ ptn) $ lines lns
 
 main = do
   state <- readFile "/proc/acpi/battery/BAT0/state"
-  info <- readFile "/proc/acpi/battery/BAT0/info"
   let current = getCapacitance "remaining capacity:" state
+  info <- readFile "/proc/acpi/battery/BAT0/info"
   let full = getCapacitance "design capacity:" info 
   print $ show (floor $ current / full * 100) ++ "%"
 
